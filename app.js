568ddcbbb54a4d7cfd0e590b277793cfe7825209
PritiSalvi1 <pritisalvi10603@gmail.com>
2025-04-13 19:14:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello from Node.js! -Priti');
-});
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
+app.get('/', (req, res) => {
+    res.send('Hello from Github NodeJs!  This is DevOps Practical 5 Question no 5 - Priti');
+});
+setTimeout(() => {
+    server.close(() => {
+        console.log('Server stopped after 30 seconds');
+    });
+}, 30000); 
